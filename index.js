903e021ee3b8de716677697a7d5d501ecab7a5e9
endou18 <73010363+endou18@users.noreply.github.com>
2023-07-20 23:22:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var a=Math.floor(Math.random() * 6)+1;
-var b="images/dice"+a+".png";
+var b="dice"+a+".png";
 document.querySelectorAll("img")[0].setAttribute("src",b);
 var c=Math.floor(Math.random() * 6)+1;
-var d="images/dice"+c+".png";
+var d="dice"+c+".png";
 document.querySelectorAll("img")[1].setAttribute("src",d);
 if(a>c){
     document.querySelector("h1").textContent="Player 1 wins";
